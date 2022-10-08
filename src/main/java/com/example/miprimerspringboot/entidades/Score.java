23857e2ee4033504834c983c977d8ecbc192d9ae
Tatiana Suarez <tsuarez@bv.studio>
2022-10-07 19:50:10 -0500

    categoria y clientes

--- src/main/java/com/example/miprimerspringboot/entidades/Score.java
+++
@@ -1,4 +0,0 @@
-package com.example.miprimerspringboot.entidades;
-
-public class Score {
-}
--- src/main/java/com/example/miprimerspringboot/repository/CRUDRepository/CategoryCRUDRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.miprimerspringboot.repository.CRUDRepository;
-
-import com.example.miprimerspringboot.entidades.Category;
-import com.example.miprimerspringboot.entidades.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryCRUDRepository extends CrudRepository<Category,Integer> {
-}
